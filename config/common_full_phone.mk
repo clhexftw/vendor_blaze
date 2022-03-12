178a371ea5ac5832181acf3b31b2c57c293dc6cd
anupam41 <anupamsamanta41@gmail.com>
2022-03-12 23:50:19 +0000

    vendor: Welcome to ProjectBlaze
    Signed-off-by: anupam41 <anupamsamanta41@gmail.com>

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,15 +1,15 @@
-# Inherit full common elytra stuff
-$(call inherit-product, vendor/elytra/config/common_full.mk)
+# Inherit full common blaze stuff
+$(call inherit-product, vendor/blaze/config/common_full.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \
     LatinIME
 
-# Include elytra LatinIME dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/elytra/overlay/dictionaries
+# Include blaze LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/blaze/overlay/dictionaries
 
 # Enable support of one-handed mode
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode=true
 
-$(call inherit-product, vendor/elytra/config/telephony.mk)
+$(call inherit-product, vendor/blaze/config/telephony.mk)
